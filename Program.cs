28a6d8d214caa51b129f75ceb1311aa1e16aa8e9
CodingApe9 <pos06098@gmail.com>
2023-03-25 16:55:09 +0900

    20230325-1

--- Program.cs
+++ Program.cs
@@ -1,30 +1,68 @@
-﻿using LinkedList.Stack;
+﻿using Test.report1_20230327_stack;
 
 //StarDiamond main = new StarDiamond();
 //main.run();
 
 //int LinkedList Stack Test
-LinkedListStack<int> intStack = new LinkedListStack<int>();
-intStack.Push(10);
-intStack.Push(20);
-intStack.Push(30);
-intStack.Push(40);
-Console.WriteLine(intStack.Pop());
-Console.WriteLine(intStack.Peek());
-intStack.Clear();
-Console.WriteLine(intStack.Peek());
-Console.WriteLine(intStack.Size());
+Console.WriteLine("-----------------------------------------------------");
+Console.WriteLine("#int LinkedList Stack Test Start");
+LinkedListStack<int> intLinkedListStack = new LinkedListStack<int>();
+intLinkedListStack.Push(10);
+intLinkedListStack.Push(20);
+intLinkedListStack.Push(30);
+intLinkedListStack.Push(40);
+Console.WriteLine(intLinkedListStack.Pop());
+Console.WriteLine(intLinkedListStack.Peek());
+intLinkedListStack.Clear();
+Console.WriteLine(intLinkedListStack.Peek());
+Console.WriteLine(intLinkedListStack.Size());
+Console.WriteLine("#int LinkedList Stack Test End");
+Console.WriteLine("-----------------------------------------------------");
 
 //string LinkedList Stack Test
-LinkedListStack<string> stringStack = new LinkedListStack<string>();
-stringStack.Push("aaa");
-stringStack.Push("bb");
-stringStack.Push("ccc");
-stringStack.Push("dddddd");
-stringStack.Push("zzzzz");
-Console.WriteLine(stringStack.Pop());
-Console.WriteLine(stringStack.Peek());
-stringStack.Clear();
-Console.WriteLine(stringStack.Peek());
-Console.WriteLine(stringStack.Size());
+Console.WriteLine("-----------------------------------------------------");
+Console.WriteLine("#string LinkedList Stack Test Start");
+LinkedListStack<string> stringLinkedListStack = new LinkedListStack<string>();
+stringLinkedListStack.Push("aaa");
+stringLinkedListStack.Push("bb");
+stringLinkedListStack.Push("ccc");
+stringLinkedListStack.Push("dddddd");
+stringLinkedListStack.Push("zzzzz");
+Console.WriteLine(stringLinkedListStack.Pop());
+Console.WriteLine(stringLinkedListStack.Peek());
+stringLinkedListStack.Clear();
+Console.WriteLine(stringLinkedListStack.Peek());
+Console.WriteLine(stringLinkedListStack.Size());
+Console.WriteLine("#string LinkedList Stack Test End");
+Console.WriteLine("-----------------------------------------------------");
 
+//int Array Stack Test
+Console.WriteLine("-----------------------------------------------------");
+Console.WriteLine("#int Array Stack Test Start");
+ArrayStack<int> intArrayStack = new ArrayStack<int>(10);
+intArrayStack.Push(10);
+intArrayStack.Push(20);
+Console.WriteLine(intArrayStack.Pop());
+Console.WriteLine(intArrayStack.Peek());
+intArrayStack.Push(444);
+Console.WriteLine(intArrayStack.Peek());
+intArrayStack.Clear();
+Console.WriteLine(intArrayStack.Pop());
+Console.WriteLine("#int Array Stack Test End");
+Console.WriteLine("-----------------------------------------------------");
+
+//string Array Stack Test
+Console.WriteLine("-----------------------------------------------------");
+Console.WriteLine("#string Array Stack Test Start");
+ArrayStack<string> stringArrayStack = new ArrayStack<string>(10);
+stringArrayStack.Push("asdf");
+stringArrayStack.Push("hhh241f");
+stringArrayStack.Push("9237dsgsgsd");
+Console.WriteLine(stringArrayStack.Pop());
+Console.WriteLine(stringArrayStack.Peek());
+stringArrayStack.Push("aaaaaaa");
+Console.WriteLine(stringArrayStack.Peek());
+stringArrayStack.Clear();
+Console.WriteLine(stringArrayStack.Pop());
+Console.WriteLine("#string Array Stack Test End");
+Console.WriteLine("-----------------------------------------------------");
---
+++ report1_20230327_stack/ArrayStack.cs
@@ -0,0 +1,53 @@
+﻿namespace Test.report1_20230327_stack
+{
+    internal class ArrayStack<T>
+    {
+        T[] array;
+        int top, maxSize;
+        public ArrayStack(int size)
+        {
+            this.maxSize = size;
+            this.array = new T[size];
+            this.top = -1;
+        }
+        public void Push(T value)
+        {
+            if (top+1 == maxSize)
+            {
+                Console.WriteLine("full Stack");
+                return;
+            }
+            array[++top] = value;
+        }
+        public T Pop()
+        {
+            if (top == -1)
+            {
+                Console.WriteLine("Empty Stack");
+                return default(T);
+            }
+            return array[top--];
+        }
+        public T Peek()
+        {
+            if (top == -1)
+            {
+                Console.WriteLine("Empty Stack");
+                return default(T);
+            }
+            return array[top];
+        }
+        public int Size()
+        {
+            if (top == -1)
+            {
+                Console.WriteLine("Empty Stack");
+            }
+            return top;
+        }
+        public void Clear()
+        {
+            this.top = -1;
+        }
+    }
+}
